bcc20b05930fc7d777802c1fdf214a43c2796912
kumapachi <makukuma52@gmail.com>
2023-01-11 00:44:07 +0900

    ActiveHashの実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,9 +1,23 @@
 class ItemsController < ApplicationController
   def index
-    @items = Item.all
+    @items = Item.order("created_at DESC")
   end
 
   def new
     @item = Item.new
+  end
+
+  def create
+    Item.create(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+  def item_params
+    params.require(:item).permit(:item_name, :item_info, :item_category_id, :item_condition_id, :shipping_fee_id, :prefecture_id, :scheduled_delivery_id, :price).merge(user_id: current_user.id)
   end
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,3 +1,20 @@
 class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+
   belongs_to :user
+
+  belongs_to :item_category
+  belongs_to :item_condition
+  belongs_to :shipping_fee
+  belongs_to :prefecture
+  belongs_to :scheduled_delivery
+
+  with_options presence: true do
+    validates :item_name, :item_info
+    validates :item_category
+  end
+  with_options numericality: { other_than: 1 , message: "can't be blank"} do
+    validates :item_category_id, :item_condition, :shipping_fee, :prefecture, :scheduled_delivery
+  end
+
 end
