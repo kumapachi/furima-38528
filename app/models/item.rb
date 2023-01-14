ed6407f3096eeb546288822da4bb84c92aa3f77b
kumapachi <makukuma52@gmail.com>
2023-01-14 14:58:43 +0900

    モデルの単体テストコード追加

--- app/models/item.rb
+++ app/models/item.rb
@@ -12,10 +12,10 @@ class Item < ApplicationRecord
 
   with_options presence: true do
     validates :image, :item_name, :item_info
-    validates :item_category, :item_condition, :shipping_fee, :prefecture, :scheduled_delivery
+    validates :item_category_id, :item_condition_id, :shipping_fee_id, :prefecture_id, :scheduled_delivery_id
     validates :price, numericality: {only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999 }
   end
-  with_options numericality: { other_than: 1 , message: "can't be blank"} do
+  with_options numericality: { other_than: 1 } do
     validates :item_category_id, :item_condition_id, :shipping_fee_id, :prefecture_id, :scheduled_delivery_id
   end
 
