6cb692ece53d59474db656d5b10335d88a0075af
kumapachi <makukuma52@gmail.com>
2023-01-16 23:48:06 +0900

    コントローラーへ編集アクション追加

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,6 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, except: [:index, :show]
+  before_action :authenticate_user!, only: [:new, :edit]
+  before_action :move_to_index, except: [:index, :show]
 
   def index
     @items = Item.order('created_at DESC')
@@ -23,10 +24,32 @@ class ItemsController < ApplicationController
     @item = Item.find(params[:id])
   end
 
+  def edit
+    @item = Item.find(params[:id])
+    unless user_signed_in? && current_user.id == @item.user_id
+    redirect_to root_path
+    end
+  end
+
+  def update
+    @item = Item.find(params[:id])   
+    if @item.update(item_params)
+      redirect_to item_path(item_params), method: :patch
+    else
+      render :edit
+    end
+  end
+
   private
 
   def item_params
     params.require(:item).permit(:image, :item_name, :item_info, :item_category_id, :item_condition_id, :shipping_fee_id,
                                  :prefecture_id, :scheduled_delivery_id, :price).merge(user_id: current_user.id)
+  end
+
+  def move_to_index
+    unless user_signed_in?
+      redirect_to action: :index
+    end
   end
 end
