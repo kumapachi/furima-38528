738987eb099e90ae8ef96af7ad45fa79505106e1
kumapachi <makukuma52@gmail.com>
2023-01-22 10:47:19 +0900

    商品購入のFormオブジェクト設定

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,6 +1,28 @@
 class OrdersController < ApplicationController
+  before_action :purchased_item, only: [:index, :create]
 
   def index
+    @order_buyer = OrderBuyer.new
   end
-  
+
+  def create
+    @order_buyer = OrderBuyer.new(order_params)
+    if @order_buyer.valid?
+      @order_buyer.save
+      redirect_to root_path
+    else
+      render :index
+    end
+  end
+
+  private
+
+  def order_params
+    params.require(:order_buyer).permit(:postal_code, :prefecture_id, :city, :addresses, :building, :phone_number).merge(user_id: current_user.id, item_id: params[:item_id])
+  end
+
+  def purchased_item
+    @item = Item.find(params[:item_id])
+  end
+
 end
---
+++ app/models/order_buyer.rb
@@ -0,0 +1,24 @@
+class OrderBuyer
+  include ActiveModel::Model
+  attr_accessor :user_id, :order_id, :postal_code, :prefecture_id, :city, :addresses, :building, :phone_number
+
+  with_options presence: true do
+    # orderモデルのバリデーション
+    validates :user_id
+    validates :item_id
+    # buyerモデルのバリデーション
+    validates :postal_code, format: { with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
+    validates :prefecture_id, numericality: { other_than: 1, message: "can't be blank" }
+    validates :city
+    validates :addresses
+    validates :phone_number, format: { with: /\A[0-9]{11}\z/, message: 'is invalid' }
+  end
+
+  def save
+    # 購入情報を保存し、変数orderに代入する
+    order = Order.create(user_id: user_id, item_id: item_id)
+    # 購入者情報を保存し、order.idと紐付ける
+    Buyer.create(order_id: order.id, postal_code: postal_code, prefecture_id: prefecture_id, city: city, addresses: addresses, building: building, phone_number: phone_number)
+  end
+
+end
