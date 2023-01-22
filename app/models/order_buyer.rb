9985b384b2178ce3ba9b309b1f92f26626c30986
kumapachi <makukuma52@gmail.com>
2023-01-22 22:33:01 +0900

    購入者情報登録機能エラーの修正

--- app/models/order_buyer.rb
+++ app/models/order_buyer.rb
@@ -1,6 +1,6 @@
 class OrderBuyer
   include ActiveModel::Model
-  attr_accessor :user_id, :order_id, :postal_code, :prefecture_id, :city, :addresses, :building, :phone_number
+  attr_accessor :user_id, :item_id, :postal_code, :prefecture_id, :city, :addresses, :building, :phone_number
 
   with_options presence: true do
     # orderモデルのバリデーション
