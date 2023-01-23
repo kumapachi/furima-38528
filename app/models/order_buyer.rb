2e1a5073b9b3c336c32c1f39f92cc9c088b16770
kumapachi <makukuma52@gmail.com>
2023-01-23 21:51:09 +0900

    エラーハンドリングの追加

--- app/models/order_buyer.rb
+++ app/models/order_buyer.rb
@@ -7,11 +7,12 @@ class OrderBuyer
     validates :user_id
     validates :item_id
     # buyerモデルのバリデーション
-    validates :postal_code, format: { with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
+    validates :postal_code, format: { with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Enter it as follows (e.g. 123-4567)"}
     validates :prefecture_id, numericality: { other_than: 1, message: "can't be blank" }
     validates :city
     validates :addresses
-    validates :phone_number, format: { with: /\A[0-9]{11}\z/, message: 'is invalid' }
+    validates :phone_number, format: { with: /\A[0-9]{11}\z/, message: 'is invalid. Input only number' },
+                             length: { minimum: 10, message: 'is too short' }
   end
 
   def save
